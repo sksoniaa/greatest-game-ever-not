9a779e5ec29a1aa5db50eda321e430cfe16ade54
Sofiia Kosachevych <sofiakosachvych@gmail.com>
2024-02-05 16:30:50 -0600

    html and css changes, + new function for the start of the game

--- js/app.js
+++ js/app.js
@@ -1,20 +1,62 @@
 const scoreEl = document.querySelector('.score');
 const timerEl = document.querySelector('.timer-display');
-const startBtnEl = document.querySelector('.start-button');
-
-const cards = ['🥰','🥰', '🧚‍♀️', '🧚‍♀️', '🤩', '🤩', '👩🏼‍💻', '👩🏼‍💻', '🤍', '🤍', '👾', '👾'];
-
-let shuffleCards = cards.sort(() => (Math.random() > .5) ? 2 : -1);
-cards.forEach(function(card) {
-  let cardEl = document.createElement('div');
-  cardEl.className = 'card-block';
-  cardEl.innerHTML = card;
-  document.querySelector('.board').appendChild(cardEl)
-})
+const restartBtnEl = document.querySelector('.restart-button');
+const startBtnEl = document.querySelector('.start-button')
+let boardEl = document.querySelector('.board')
 
 const hasFlippedCards = false;
 let firstPickedCard;
 let secondPickedCard;
+
+const cards = ['🥰','🥰', '🧚‍♀️', '🧚‍♀️', '🤩', '🤩', '👩🏼‍💻', '👩🏼‍💻', '🤍', '🤍', '👾', '👾'];
+let shuffleCards = cards.sort(() => (Math.random() > .5) ? 2 : -1);
+
+
+
+function startTheGame() {
+  boardEl.innerText = '';
+  boardEl.classList.remove('background')
+  cards.forEach(function(card) {
+    let cardEl = document.createElement('div');
+    cardEl.className = 'card-block';
+    cardEl.innerHTML = card;
+    cardEl.addEventListener('click', function() {
+      this.classList.add('cardFront')
+      console.log(this)
+    })
+    boardEl.appendChild(cardEl)
+  })
+}
+
+ startBtnEl.addEventListener('click', startTheGame)
+
+// function startTheGame() {
+//   boardEl.forEach(function(card) {
+//     cardEl.addEventListener('click', function() {
+//     this.classList.add('cardFront')
+//     console.log('started');
+//   })
+//   })
+  
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // const timerMin = 5;
