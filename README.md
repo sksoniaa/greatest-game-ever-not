255843cae754ffa0c68a041a005af3ab189e1190
Sofiia Kosachevych <sofiakosachvych@gmail.com>
2024-02-08 15:32:05 -0600

    completed project, changed function for fiding a match, modals about the results appear at the end of the game, readme file is written

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-This is going to be my game's readme file.
+Hello! Welcome to Memory Match.
+
+Click the button Start to start playing the game.
+Your task is to find all the matching cards.
+On the top of the page you can see the timer and the number of score. You have 5 minutes to win!
+Have fun!
--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
 const scoreEl = document.querySelector(".score");
-let score = 0;
-let tries = 0;
 const timerEl = document.querySelector(".timer-display");
-const timerMin = 5;
-let time = timerMin * 60;
 const restartBtnEl = document.querySelector(".restart-button");
 const startBtnEl = document.querySelector(".start-button");
-let boardEl = document.querySelector(".board"); //parent
-const children = boardEl.children;
 const pickedCardsClass = document.getElementsByClassName("cardFront");
+let boardEl = document.querySelector(".board"); //parent
+let vPopupEl = document.querySelector('.v-popup')
+let fPopupEl = document.querySelector('.f-popup')
+let playAgainBtn = document.querySelector('.play-again-btn')
+let tryAgainBtn = document.querySelector('.try-again-btn')
+let containerEl = document.querySelector('.container')
+let aEl = document.querySelector('a')
+let aImgEl = document.querySelector('.a-img')
+const children = boardEl.children;
+let thisEl;
+let score = 0;
 let pickedArr = [];
-let text;
 let firstPickedCard;
 let secondPickedCard;
+let timerInterval;
 
 //create an array of cards
 
@@ -32,79 +37,129 @@ const cards = [
 ];
 let shuffleCards = cards.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
-// start the game
 
+// start the game when clicked on the btn Start
 startBtnEl.addEventListener("click", startTheGame);
 let cardEl;
-function startTheGame() {
+
+// function that removes the initial div saying welcome...
+function removeWelcomePage() {
   boardEl.innerText = "";
   boardEl.classList.remove("background");
+}
+
+// function to start the game
+function startTheGame() {
+  removeWelcomePage()
   //start the timer
-  setInterval(updateTimer, 1000);
-  // create a variable for each card
+  timerInterval = setInterval(updateTimer, 1000);
+  // define a variable for each card
   cards.forEach(function (card) {
     cardEl = document.createElement("div");
     cardEl.className = "card-block";
     cardEl.innerHTML = card;
     boardEl.appendChild(cardEl);
-    play();
+    play()
   });
 }
-function play() {
-  cardEl.addEventListener("click", function () {
-    tries++;
-    console.log(tries);
-    pickedArr.push(this.textContent);
-
-    this.classList.add("cardFront");
-
-    if (pickedCardsClass.length >= 2 && pickedCardsClass.length % 2 === 0) {
-      disable();
-      checkForMatch();
-      makeAble();
-    }
-  });
-  if (score === 6) {
-    victory();
-  } else if ((timerEl.innerHTML = "0:00")) {
-    fail();
-  }
-}
-
-function disable() {
-  for (let i = 0; i < children.length; i++) {
-    children[i].classList.add("disabled");
-  }
-}
-
-function makeAble() {
-  for (let i = 0; i < children.length; i++) {
-    children[i].classList.remove("disabled");
+// flipping
+let lastPicked = [];
+function flipTheCardFront() {
+  thisEl = this
+  thisEl.classList.add("cardFront");
+  
+  pickedArr.push(thisEl);
+  if (pickedArr.length % 2 !== 0) {
+    play()
+  }else if (pickedArr.length >= 2 && pickedArr.length % 2 === 0) {
+    thisEl = this
+    thisEl.classList.add('cardFront')
+    firstPickedCard = pickedArr[pickedArr.length-2]
+    secondPickedCard = pickedArr[pickedArr.length-1]
+    lastPicked = [firstPickedCard, secondPickedCard]
+    checkForMatch()
   }
 }
 
 function checkForMatch() {
-  firstPickedCard = pickedArr[pickedArr.length - 2];
-  secondPickedCard = pickedArr[pickedArr.length - 1];
-  console.log(firstPickedCard);
-  console.log(secondPickedCard);
-  if (firstPickedCard === secondPickedCard) {
-    score++;
-    console.log(`score is ${score}`);
-  } else {
-    console.log(score + " you didn't guess");
-  }
-  scoreEl.innerHTML = `Score: ${score}`;
+  if (firstPickedCard.innerText !== secondPickedCard.innerText) {
+    setTimeout(() => {
+      firstPickedCard.classList.remove('cardFront');
+      secondPickedCard.classList.remove('cardFront')
+    }, 1000)
+} else if (firstPickedCard.innerText === secondPickedCard.innerText) {
+  firstPickedCard.classList.add('disabled')
+  secondPickedCard.classList.add('disabled')
+  score ++;
+}
+scoreEl.innerHTML = `Score: ${score}`;
+ winOrLost()
 }
 
+function play() {
+  cardEl.addEventListener('click', flipTheCardFront)
+}  
+  
+// function checks if the user won or lost
+function winOrLost() {
+  if (score === 6) {
+    victory();
+  } else if (timerEl.innerHTML === "0:00") {
+    fail();
+  }
+}
+  
+// the timer
+let timerMin = 2.5;
+let time = timerMin * 60;
 function updateTimer() {
   const minutes = Math.floor(time / 60);
   let seconds = time % 60;
   seconds = seconds < 10 ? "0" + seconds : seconds;
   timerEl.innerHTML = `${minutes}:${seconds}`;
-  time--;
+  // if the timer reaches 0 => the user lost
+  if (time === 0) {
+     fail()
+  } else {
+    time--;
+  }
+}
+function victory() {
+  containerEl.classList.add('blured-container')
+  vPopupEl.classList.add('open-popup');
+  clearInterval(timerInterval)
+}
+function fail() {
+  containerEl.classList.add('blured-container')
+  fPopupEl.classList.add('open-popup');
+  clearInterval(timerInterval)
+  
+}
+playAgainBtn.addEventListener('click', removeVPopup)
+function removeVPopup() {
+  vPopupEl.classList.remove('open-popup')
+  window.location.reload()
+}
+tryAgainBtn.addEventListener('click', removeFPopup)
+function removeFPopup() {
+  fPopupEl.classList.remove('open-popup')
+  window.location.reload()
 }
 
-function victory() {}
+function showInstagram() {
+  aImgEl.classList.add('make-bigger')
+}
 
-function fail() {}
+aEl.addEventListener('mouseover', showInstagram)
+
+aEl.addEventListener('mouseout', hideInstagram)
+
+function hideInstagram() {
+  setTimeout(() => {
+    aImgEl.classList.remove('make-bigger')
+  }, 1000)
+}
+
+aImgEl.addEventListener('click', function() {
+  window.open("https://www.instagram.com/sksoniaa_/")
+})
